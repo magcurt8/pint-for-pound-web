a5be82abbcc521cfdb6a9a4a2af0e44726e268e4
magcurt8 <mcurtis@yodle.com>
2019-01-30 23:52:31 -0500

    initializing db

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
-const os = require('os');
+const bodyParser = require('body-parser');
+const http = require('http');
 
+// const mongoose = require('mongoose');
+
+const router = express.Router();
 const app = express();
+const port = process.env.PORT || 8080;
+
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+// connect to database
+// mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o');
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', (req, res) => {
+  res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
+// app.listen(port);
+console.log(`Magic happens on port ${port}`);
